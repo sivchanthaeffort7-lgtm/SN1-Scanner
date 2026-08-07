b6da47b908f523233e5ba6081ed4a98f8e2605a2
sivchanthaeffort7-lgtm <sivchantha.effort7@gmail.com>
2026-08-06 21:31:32 -0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,24 @@
-//sn1 login javascript
+const toggle = document.getElementById("togglePassword");
+const password = document.getElementById("pass");
+
+if (toggle && password) {
+
+    toggle.addEventListener("click", function () {
+
+        if (password.type === "password") {
+
+            password.type = "text";
+
+            toggle.classList.replace("fa-eye", "fa-eye-slash");
+
+        } else {
+
+            password.type = "password";
+
+            toggle.classList.replace("fa-eye-slash", "fa-eye");
+
+        }
+
+    });
+
+}
